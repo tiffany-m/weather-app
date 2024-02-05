f08e93a8acd220df39f768abaef5d34cd0845c84
tiffany-m <67019104+tiffany-m@users.noreply.github.com>
2024-02-05 10:41:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Title
+Quick Description
+
+## Directions
+- "npm install" to install dependences in package.json
+-  Commands to run
+  -  npx webpack
+  -  npx webpack –-watch (continuously update, ctrl + c to stop)
+  -  npm run build 
+
+## Build Status
+
+## Tech/Framework
+- HTML
+- CSS
+- JavaScript
+- Webpack
+
+## API Used
+
+## Known Bugs
+
+## Motivation
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
