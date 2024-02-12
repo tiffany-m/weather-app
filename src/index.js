4bcee0900e228f0b6a2fc15ff6c0c1160e94eb83
Tiffany <67019104+tiffany-m@users.noreply.github.co>
2024-02-12 17:20:59 -0500

    Fixing margins and padding

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.css";
-import tempLogo from "./images/templogo.png";
+import appLogo from "./images/templogo.png";
+import magnifyingGlass from "./images/search.png";
 import { getCurrentWeather } from "./components/currentWeather";
 import { getFutureWeather } from "./components/furtureWeather";
 
@@ -8,7 +9,9 @@ const searchBar = document.getElementById("searchbar");
 let location = "Broomall";
 
 const logo = document.getElementById("logo");
-logo.src = tempLogo;
+logo.src = appLogo;
+const searchButton = document.getElementById("search-btn");
+searchButton.src = magnifyingGlass;
 
 getCurrentWeather(location);
 getFutureWeather(location);
@@ -16,6 +19,7 @@ getFutureWeather(location);
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   location = searchBar.value;
+  searchBar.value = "";
 
   getCurrentWeather(location);
   getFutureWeather(location);
